2a96f3f6a29e6ff13d27a780ae00d478c254b14f
canikwe <c.anikwe@gmail.com>
2019-03-05 15:03:43 -0500

    add update action to pets controller

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,4 +1,4 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :happiness, :image, :active_status
+  attributes :id, :name, :age, :happiness, :image, :active_status
   has_many :characteristics
 end
