35421fd1273caff615ad78db3a4c6beb2d6f7561
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-03-05 10:22:21 -0500

    add post new pet to backend

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -8,4 +8,24 @@ class Api::V1::PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     render json: @pet
   end
+
+  def create
+    @pet = Pet.new
+    @pet = Pet.create(pet_params)
+    if @pet.save
+      PetCharacteristic.create(pet_id: @pet.id, characteristic_id: 1)
+      PetCharacteristic.create(pet_id: @pet.id, characteristic_id: 2)
+      PetCharacteristic.create(pet_id: @pet.id, characteristic_id: 3)
+      render json: @pet, status: :accepted
+    else
+      render json: {errors: @pet.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :image, :happiness, :active_status)
+  end
+
 end
