d37ab716c82afa49e5c5dff64dbf92fc5c0548eb
canikwe <c.anikwe@gmail.com>
2019-03-05 17:20:41 -0500

    add kill_clock

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,10 @@
 class Pet < ApplicationRecord
   has_many :pet_characteristics
   has_many :characteristics, through: :pet_characteristics
+
+  def kill_clock
+    self.created_at + (60 * 5)
+  end
+
+
 end
--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,4 +1,4 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :happiness, :image, :active_status, :bio
+  attributes :id, :name, :age, :happiness, :image, :active_status, :bio, :kill_clock
   has_many :characteristics
 end
