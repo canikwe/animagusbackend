2fbc1c2c8a3f244eae7dbb4d3df7a73efe2f02e5
canikwe <c.anikwe@gmail.com>
2019-03-08 12:48:22 -0500

    fix feed bug

--- app/models/pet_characteristic.rb
+++ app/models/pet_characteristic.rb
@@ -4,25 +4,25 @@ class PetCharacteristic < ApplicationRecord
 
   def calculate_check_time
 
-    if self.characteristic.name == "Hunger"
-      if self.check_time == nil
-        Time.now + self.characteristic.interval
-      else
-        self.check_time + self.characteristic.interval
-      end
-    elsif self.characteristic.name == "Thirst"
-      if self.check_time == nil
-        Time.now + self.characteristic.interval
-      else
-        self.check_time + self.characteristic.interval
-      end
-      # self.created_at + (60 * 4)
-    elsif self.characteristic.name == "Sleepiness"
-      if self.check_time == nil
-        Time.now + self.characteristic.interval
-      else
-        self.check_time + self.characteristic.interval
-      end
-    end
+    # if self.characteristic.name == "Sleep"
+    #   if self.check_time == nil
+    #     Time.now + self.characteristic.interval
+    #   else
+    #     self.check_time + self.characteristic.interval
+    #   end
+    # elsif self.characteristic.name == "Thirst"
+    #   if self.check_time == nil
+    #     Time.now + self.characteristic.interval
+    #   else
+    #     self.check_time + self.characteristic.interval
+    #   end
+    #   # self.created_at + (60 * 4)
+    # elsif self.characteristic.name == "Sleepiness"
+    #   if self.check_time == nil
+    #     Time.now + self.characteristic.interval
+    #   else
+    #     self.check_time + self.characteristic.interval
+    #   end
+    # end
   end
 end
