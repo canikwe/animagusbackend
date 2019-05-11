260466e83821d212082edd3585471cf9436c88f4
canikwe <c.anikwe@gmail.com>
2019-05-10 21:45:45 -0400

    remove unused code and clean up comments

--- app/models/pet_characteristic.rb
+++ app/models/pet_characteristic.rb
@@ -3,37 +3,13 @@ class PetCharacteristic < ApplicationRecord
   belongs_to :characteristic
 
   def calculate_check_time
-    
-
     if self.check_time == nil
-      #hard coding 20 seconds for testing purposes
-      # Time.now + 20
+      #converting interval milliseconds to seconds for Ruby Time.now handling
       Time.now + (self.characteristic.interval / 1000)
     else
-      #converting interval milliseconds to seconds for Ruby time handling
-      # self.check_time + 20
+      #converting interval milliseconds to seconds for Ruby Time.now handling
       self.check_time + (self.characteristic.interval / 1000)
     end
-
-    # if self.characteristic.name == "Hunger"
-    #   if self.check_time == nil
-    #     Time.now + self.characteristic.interval
-    #   else
-    #     self.check_time + self.characteristic.interval
-    #   end
-    # elsif self.characteristic.name == "Bordeom"
-    #   if self.check_time == nil
-    #     Time.now + self.characteristic.interval
-    #   else
-    #     self.check_time + self.characteristic.interval
-    #   end
-    #   # self.created_at + (60 * 4)
-    # elsif self.characteristic.name == "Cleanliness"
-    #   if self.check_time == nil
-    #     Time.now + self.characteristic.interval
-    #   else
-    #     self.check_time + self.characteristic.interval
-    #   end
-    # end
   end
+
 end
